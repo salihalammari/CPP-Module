cad0e328be1d3de9eec7a875e005f63b5b0cff4a
saliha <slammari@student.1337.ma>
2022-11-28 06:22:00 +0100

    is done

--- CPP-module/Module 01/ex06/Harl.cpp
+++ CPP-module/Module 01/ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: slammari <slammari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 04:55:09 by slammari          #+#    #+#             */
-/*   Updated: 2022/11/25 04:55:10 by slammari         ###   ########.fr       */
+/*   Updated: 2022/11/28 06:21:09 by slammari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,6 @@ Harl::Harl( void )
 
 Harl::~Harl( void )
 {
-}
-
-void Harl::debug(void)
-{
-    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
-}
-
-void Harl::info(void)
-{
-    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
-}
-
-void Harl::warning(void)
-{
-    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void	Harl::complain( std::string level )
