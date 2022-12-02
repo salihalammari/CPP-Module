10346359ff7631113cf3851fc827dfd3c215ce87
saliha <slammari@student.1337.ma>
2022-12-02 20:35:09 +0100

    done last modified

--- CPP-module/Module 01/ex04/main.cpp
+++ CPP-module/Module 01/ex04/main.cpp
@@ -6,14 +6,13 @@
 /*   By: slammari <slammari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 21:38:15 by slammari          #+#    #+#             */
-/*   Updated: 2022/11/29 16:22:57 by slammari         ###   ########.fr       */
+/*   Updated: 2022/12/02 20:34:28 by slammari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 
-<<<<<<< HEAD
 
 void replace(std::string namefile, std::fstream &file, std::string s1, std::string s2)
 {
@@ -44,27 +43,11 @@ void replace(std::string namefile, std::fstream &file, std::string s1, std::stri
         }
         newfile << str << std::endl;
     }
-=======
-std::string replace(std::string s1, std::string s2, std::string line)
-{
-    int i;
-
-    if (s1.empty() == true)
-        return line;
-    i = line.find(s1);
-    while (i != std::string::npos)
-    {
-    	line.erase(i, s1.length());
-        line.insert(i, s2);
-        i = line.find(s1, i + s2.length());
-    }
-    return line;
->>>>>>> f25b9f0b3eaf55d13d8242f79da482d79c2c77c1
-}
+    
 
 int main(int ac, char **av)
 {
-<<<<<<< HEAD
+
     std::fstream file;
     if (ac == 4)
     {
@@ -77,50 +60,4 @@ int main(int ac, char **av)
         replace(av[1], file, av[2], av[3]);
     }
     return(0);
-=======
-    if (ac < 4)
-    {
-        std::cout << "you entred less than three arguments." << std::endl;
-        return (1);
-    }
-    else if (ac > 4)
-    {
-        std::cout << "you entred more than three arguments." << std::endl;
-        return (1);
-    }
-    std::string filename = av[1];
-    std::string s1 = av[2];
-    std::string s2 = av[3];
-    std::string line;
-    std::ifstream infile;
-    std::ofstream outfile;
-
-    infile.open(filename, std::ios::in);
-    if (infile.is_open() == true)
-    {
-        outfile.open(filename + ".replace", std::ios::out | std::ios::trunc);
-        if (outfile.is_open() == true)
-        {
-            while (std::getline(infile, line))
-            {
-                line = my_replace(s1, s2, line);
-                outfile << line << std::endl;
-            }
-            outfile.close();
-        }
-        else
-        {
-            std::perror("outfile error.");
-            infile.close();
-            return (1);
-        }
-        infile.close();
-    }
-    else
-    {
-        std::perror("infile error.");
-        return (1);
-    }
-    return (0);
->>>>>>> f25b9f0b3eaf55d13d8242f79da482d79c2c77c1
 }
---
+++ CPP-module/Module 03/ex03/ScavTrap.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slammari <slammari@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/01 21:58:42 by slammari          #+#    #+#             */
+/*   Updated: 2022/12/01 21:58:43 by slammari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+#include <string>
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+public :
+	bool guard_mode;
+	ScavTrap(void);
+	ScavTrap(std::string name);
+	ScavTrap(const ScavTrap& scav);
+	~ScavTrap(void);
+	ScavTrap& operator=(const ScavTrap& scav);
+
+	void attack(const std::string& target);
+	void takeDamage(unsigned int amount);
+	void guardGate(void);
+};
+
+#endif
