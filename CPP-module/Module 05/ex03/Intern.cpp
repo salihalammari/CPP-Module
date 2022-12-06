7839312434759d99d05e2055722ed1c79375e2df
saliha lammari <75095222+salihalammari@users.noreply.github.com>
2022-12-06 05:37:51 +0100

    create inter class

--- CPP-module/Module 05/ex03/Intern.cpp
+++ CPP-module/Module 05/ex03/Intern.cpp
@@ -10,3 +10,51 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Intern.hpp"
+
+Intern::Intern(void) {}
+
+Intern::~Intern(void) {}
+
+Intern::Intern(const Intern& i)
+{
+	static_cast<void>(i);
+}
+
+Intern& Intern::operator=(const Intern& i)
+{
+	static_cast<void>(i);
+	return *this;
+}
+
+Form* Intern::CreateShrubberyCreationForm(const std::string& target)
+{
+	std::cout << "Intern creates shrubbery creation form. (" << target << ')' << std::endl;
+	return new ShrubberyCreationForm(target);
+}
+
+Form* Intern::CreateRobotomyForm(const std::string& target)
+{
+	std::cout << "Intern creates robotomy request form. (" << target << ')' << std::endl;
+	return new RobotomyRequestForm(target);
+}
+
+Form* Intern::CreatePresidentialPardonForm(const std::string& target)
+{
+	std::cout << "Intern creates presidential pardon form. (" << target << ')' << std::endl;
+	return new PresidentialPardonForm(target);
+}
+
+Form* Intern::makeForm(const std::string& name, const std::string& target)
+{
+	std::string form[3] = { "shrubbery creation", "robotomy request", "presidential pardon" };
+	Form *(Intern::*func_ptr[3])(const std::string &target) =
+		{ &Intern::CreateShrubberyCreationForm, &Intern::CreateRobotomyForm, &Intern::CreatePresidentialPardonForm };
+	for (int i = 0 ; i < 3 ; i++)
+	{
+		if (form[i] == name)
+			return (this->*func_ptr[i])(target);
+	}
+	std::cout << "No form exists for the given name." << std::endl;
+	return (NULL);
+}
--- CPP-module/Module 05/ex03/Intern.hpp
+++ CPP-module/Module 05/ex03/Intern.hpp
@@ -10,3 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef INTERN_HPP
+#define INTERN_HPP
+
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
+
+class Intern
+{
+public :
+    Intern(void);
+    Intern(const Intern& i);
+    ~Intern(void);
+    Intern& operator=(const Intern& i);
+
+    Form* CreateShrubberyCreationForm(const std::string& target);
+    Form* CreateRobotomyForm(const std::string& target);
+    Form* CreatePresidentialPardonForm(const std::string& target);
+
+    Form* makeForm(const std::string& type, const std::string& name);
+};
+
+#endif
