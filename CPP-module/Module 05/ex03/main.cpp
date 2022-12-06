8939831d22b5f506fe349936366541f15815ac0b
saliha lammari <75095222+salihalammari@users.noreply.github.com>
2022-12-07 05:11:22 +0800

    create main test

--- CPP-module/Module 05/ex03/main.cpp
+++ CPP-module/Module 05/ex03/main.cpp
@@ -10,3 +10,32 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Intern.hpp"
+
+int main(void)
+{
+	Intern someRandomIntern;
+	Form* rrf;
+
+	std::cout << "===========ShrubberyCreationForm Creation Test===========" << std::endl;
+	rrf = someRandomIntern.makeForm("shrubbery creation", "Bender");
+	delete rrf;
+	std::cout << std::endl;
+
+	std::cout << "============RobotomyRequestForm Creation Test============" << std::endl;
+	rrf = someRandomIntern.makeForm("robotomy request", "Bender");
+	delete rrf;
+	std::cout << std::endl;
+
+	std::cout << "===========PresidentialPardonForm Creation Test===========" << std::endl;
+	rrf = someRandomIntern.makeForm("presidential pardon", "Bender");
+	delete rrf;
+	std::cout << std::endl;
+
+	std::cout << "===============UndefinedForm Creation Test===============" << std::endl;
+	rrf = someRandomIntern.makeForm("undefined", "Bender");
+	delete rrf;
+	std::cout << std::endl;
+	
+	return 0;
+}
