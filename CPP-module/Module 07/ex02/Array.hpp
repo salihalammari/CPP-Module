66c281ee3f65f356795f7e222a91152e011d782d
saliha <slammari@student.1337.ma>
2022-12-09 14:48:30 +0100

    done

--- CPP-module/Module 07/ex02/Array.hpp
+++ CPP-module/Module 07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: slammari <slammari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 00:28:46 by slammari          #+#    #+#             */
-/*   Updated: 2022/12/09 00:43:30 by slammari         ###   ########.fr       */
+/*   Updated: 2022/12/09 01:14:57 by slammari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,32 @@ private :
 	int _size;
 	T* _arr;
 public :
-	Array(void) : _size(0), _arr(new T[NULL]) {}
-	Array(unsigned int n) : _size(n), _arr(new T[n])
+	Array(void) : _size(0), _arr(NULL) {}
+	Array(unsigned int n) : _size(n)
 	{
+		try
+		{
+			_arr = new T[n];
+		}
+		catch(...)
+		{
+			std::cout << "malloc error" << std::endl;
+			exit(1);
+		}
 		for (int i = 0 ; i < _size; i++)
 			_arr[i] = 0;
 	}
-	Array(const Array& a) : _size(a._size), _arr(new T[a._size])
-	{
+	Array(const Array& a) : _size(a._size)
+	{	
+		try
+		{
+			_arr = new T[a._size];
+		}
+		catch(...)
+		{
+			std::cout << "malloc error" << std::endl;
+			exit(1);
+		}
 		for (int i = 0 ; i < _size ; i++)
 			_arr[i] = a[i];
 	}
@@ -45,13 +63,21 @@ public :
 			delete[] _arr;
 			_arr = NULL;
 			_size = a._size;
-			_arr = new T[_size];
+			try
+			{
+				_arr = new T[_size];
+			}
+			catch(...)
+			{
+				std::cout << "malloc error" << std::endl;
+				exit(1);
+			}
 			for (int i = 0 ; i < _size ; i++)
 				_arr[i] = a[i];
 		}
 		return *this;
 	}
-	T& operator[](int idx)
+	T& operator[](int idx) //subscript operator
 	{
 		if (idx < 0 || idx >= _size)
 			throw OutOfBoundException();
